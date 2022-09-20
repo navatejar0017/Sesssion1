9d06eaa7af3b1f34a4da6f6be622b68bb604df0d
navatejar0017 <navatejar@cittacore.com>
2022-09-20 15:58:22 +0530

    Initial commit

---
+++ force-app/main/default/aura/accountContactsCmp/accountContactsCmpController.js
@@ -0,0 +1,12 @@
+({
+    initfun: function (cmp, event, helper) {
+        cmp.set('v.mycolumns', [
+            {label: 'Contact Name', fieldName: 'Name', type: 'text'},
+            {label: 'AccountId', fieldName: 'AccountId', type: 'text'},
+            {label: 'Phone', fieldName: 'Phone', type: 'phone'},
+            {label: 'Email', fieldName: 'Email', type: 'email'}
+        ]);
+        helper.getData(cmp);
+    }
+    
+})
---
+++ force-app/main/default/aura/accountContactsCmp/accountContactsCmpHelper.js
@@ -0,0 +1,17 @@
+({
+    getData : function(cmp) {
+        var action = cmp.get('c.getContacts');
+        action.setParams({recId:cmp.get("v.recordId")});
+        
+        action.setCallback(this, $A.getCallback(function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                cmp.set('v.mydata', response.getReturnValue());
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                console.error(errors);
+            }
+        }));
+        $A.enqueueAction(action);
+    }
+})
